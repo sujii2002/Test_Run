5962a840908230a1bceaeb6a598572452d4e4bfa
Sujitha R <sujitha-17254@sujitha-17254.csez.zohocorpin.com>
2025-06-18 11:38:18 +0530

    test

---
+++ java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test class main method");
+        int rand = Math.random();
+        System.out.println(rand);
+        String password = "RTWE1C3Y3Ew2";
+    }
+}
